12a8edb73cd3eca7db62c65d5348e8177ab007b2
has9800 <hahmed28@nait.ca>
2024-03-06 00:15:02 -0700

    sending payment to 3rd party functionality completed, working on canceling next

--- controllers/paymentsController.js
+++ controllers/paymentsController.js
@@ -1,10 +1,36 @@
 const supabase = require("../utils/supabase");
+const axios = require("axios").default;
+const {
+  pre_authorize_payment,
+  commit_payment,
+} = require("../utils/paymentHelpers/pre_authorize");
 
 const sendPayment = async (req, res) => {
-  res.status(200).send("Sending payment...");
+  try {
+    const data = await pre_authorize_payment(req);
+
+    await axios
+      .post("https://sandbox.waafipay.net/asm", data)
+      .then(async (axiosResponse) => {
+        const final_result = await commit_payment(
+          axiosResponse.data.params.transactionId,
+          axiosResponse.data.params.txAmount
+        );
+
+        res.status(200).send(final_result);
+      })
+      .catch((error) => {
+        res.status(400).send(error);
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
 };
 
 const cancelPayment = async (req, res) => {
+  // const data = await commit_payment(transaction_id, transaction_amount);
+
   res.status(200).send("Cancelling payment...");
 };
 
