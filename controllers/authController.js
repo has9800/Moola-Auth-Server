887f8c79e2b251e71ce307e8d66c8687520b8ce7
has9800 <hahmed28@nait.ca>
2024-03-05 21:45:38 -0700

    inbound encrypt/decrypt proxy server working, working on outbound next

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,13 @@
 const supabase = require("../utils/supabase");
+const evervault = require("../utils/evervault");
 
 // ------------------------------- email and password sign up
 const register = async (req, res, next) => {
+  console.log(req.body);
+
   let { error, data: user } = await supabase.auth.signUp({
-    email: req.body.email,
-    password: req.body.password,
+    email: await evervault.decrypt(req.body.email),
+    password: await evervault.decrypt(req.body.password),
   });
 
   if (error) {
@@ -46,17 +49,16 @@ const register = async (req, res, next) => {
 
 // ------------------------------- email and password sign in
 const login = async (req, res, next) => {
-  console.log(req.body);
-  // let { data, error } = await supabase.auth.signInWithPassword({
-  //   email: req.body.email,
-  //   password: req.body.password,
-  // });
+  let { data, error } = await supabase.auth.signInWithPassword({
+    email: req.body.email,
+    password: req.body.password,
+  });
 
-  // if (error) {
-  //   res.status(400).send({ error });
-  // } else {
-  //   res.status(200).send({ data });
-  // }
+  if (error) {
+    res.status(400).send({ error });
+  } else {
+    res.status(200).send({ data });
+  }
 };
 
 // ------------------------------- signout
--- controllers/endpointHealthController.js
+++ controllers/endpointHealthController.js
@@ -1,13 +1,13 @@
 const evervault = require("../utils/evervault");
 
-evervault.enableOutboundRelay();
+// evervault.enableOutboundRelay();
 
 const testEndpoint = async (req, res) => {
   const body = req.body;
 
-  const encryptedBody = await evervault.encrypt(body);
+  // const encryptedBody = await evervault.encrypt(body);
 
-  return res.status(200).send(encryptedBody);
+  return res.status(200).send(body);
 };
 
 module.exports = { testEndpoint };
