d52a7d833b48141c95d8f34b534b1eaed6fc4390
has9800 <hahmed28@nait.ca>
2024-03-16 00:37:35 -0600

    wallet functionality complete

--- controllers/walletsController.js
+++ controllers/walletsController.js
@@ -1,12 +1,34 @@
 const supabase = require("../utils/supabase");
+const evervault = require("../utils/evervault");
 
 const getCard = async (req, res) => {
-  let { data: wallets, error } = await supabase.from("wallets").select("id");
+  let { data: card, error } = await supabase
+    .from("wallets")
+    .select("*")
+    .eq("card_id", req.body.card_id);
+
+  const decryptedCard = await evervault.decrypt(card);
 
   if (error) {
     res.status(400).send({ error });
   } else {
-    res.send({ wallets });
+    res.send({ decryptedCard });
+  }
+};
+
+const getCards = async (req, res) => {
+  let { data: cards, error } = await supabase
+    .from("wallets")
+    .select("*")
+    .eq("cardholder_id", req.body.cardholder_id)
+    .range(0, 9);
+
+  const decryptedCards = await evervault.decrypt(cards);
+
+  if (error) {
+    res.status(400).send(error);
+  } else {
+    res.status(200).send(decryptedCards);
   }
 };
 
@@ -15,12 +37,16 @@ const addCard = async (req, res) => {
     .from("wallets")
     .insert([
       {
-        card_holder_id: req.body.id,
-        card_number: req.body.card_number,
-        card_cvv: req.body.card_cvv,
-        card_expiry: req.body.card_expiry,
-        card_holder_full_name: req.body.card_holder_full_name,
-        card_holder_zip_code: req.body.card_holder_zip_code,
+        cardholder_id: req.body.cardholder_id,
+        card_number: await evervault.encrypt(req.body.card_number),
+        card_cvv: await evervault.encrypt(req.body.card_cvv),
+        card_expiry: await evervault.encrypt(req.body.card_expiry),
+        cardholder_full_name: await evervault.encrypt(
+          req.body.cardholder_full_name
+        ),
+        cardholder_zip_code: await evervault.encrypt(
+          req.body.cardholder_zip_code
+        ),
       },
     ])
     .single();
@@ -32,8 +58,25 @@ const addCard = async (req, res) => {
   }
 };
 
-const removeCard = async (req, res) => {
-  res.status(200).send("Card removed...");
+const replaceCard = async (req, res) => {
+  const { data, error } = await supabase
+    .from("wallets")
+    .update({ someValue: "otherValue" })
+    .eq("card_id", req.body.card_id)
+    .single();
 };
 
-module.exports = { addCard, removeCard, getCard };
+const removeCard = async (req, res) => {
+  const { error } = await supabase
+    .from("wallets")
+    .delete()
+    .eq("card_id", req.body.card_id);
+
+  if (error) {
+    res.status(400).send(error);
+  } else {
+    res.status(200).send("Card removed successfully...");
+  }
+};
+
+module.exports = { addCard, removeCard, getCard, getCards, replaceCard };
