7a1c3d02cf3e2f4b6c44db118173c7ae3ea219f9
H.A <100562717+has9800@users.noreply.github.com>
2024-03-26 13:09:57 -0600

    Update aggregationController.js

--- controllers/aggregationController.js
+++ controllers/aggregationController.js
@@ -5,30 +5,32 @@ const { getCurrentUser } = require("../utils/authHelpers");
 const aggregateUser = async (req, res) => {
   const user = await getCurrentUser();
 
-  if (!user) res.status(200).send("No user found");
+  if (!user) {
+    res.status(200).send("No user found");
+  } else {
+    // get their cards
+    let { data: cards, error: cardsError } = await supabase
+      .from("wallets")
+      .select("*")
+      .eq("cardholder_id", user?.id)
+      .range(0, 9);
 
-  // get their cards
-  let { data: cards, error: cardsError } = await supabase
-    .from("wallets")
-    .select("*")
-    .eq("cardholder_id", user?.id)
-    .range(0, 9);
-
-  const decryptedCards = await evervault.decrypt(cards);
-
-  // get their contacts
-  let { data: contacts, error: contactsError } = await supabase
-    .from("contacts")
-    .select("*")
-    .eq("owner_id", user?.id)
-    .range(0, 9);
-
-  // get their transactions
-  let { data: transactions, error: transactionsError } = await supabase
-    .from("transactions")
-    .select("*")
-    .eq("owner_id", user?.id);
-
+    const decryptedCards = await evervault.decrypt(cards);
+  
+    // get their contacts
+    let { data: contacts, error: contactsError } = await supabase
+      .from("contacts")
+      .select("*")
+      .eq("owner_id", user?.id)
+      .range(0, 9);
+  
+    // get their transactions
+    let { data: transactions, error: transactionsError } = await supabase
+      .from("transactions")
+      .select("*")
+      .eq("owner_id", user?.id);
+    }
+  
   if (!cardsError || !contactsError || !transactionsError) {
     res.status(200).send({
       decryptedCards,
